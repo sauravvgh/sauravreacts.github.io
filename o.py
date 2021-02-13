39db7246abe4adb5815db0e3da0e8473e00d0945
saurav ghimire <48352478+sauravvgh@users.noreply.github.com>
2021-02-13 14:04:28 +0545

    Add files via upload

--- o.py
+++ o.py
@@ -1,1 +1,10 @@
 #Write a Python program to filter a list of integers using Lambda.
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print("Original list of integers:")
+print(nums)
+print("\nEven numbers from the said list:")
+even_nums = list(filter(lambda x: x%2 == 0, nums))
+print(even_nums)
+print("\nOdd numbers from the said list:")
+odd_nums = list(filter(lambda x: x%2 != 0, nums))
+print(odd_nums)
